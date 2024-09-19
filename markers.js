72a0ef111f0efb1c0b0d3e80727d0d9c00404ae7
Anthony <anthony.notgrass@gmail.com>
2024-09-18 22:04:57 -0500

    Update markers.js

--- markers.js
+++ markers.js
@@ -2,35 +2,37 @@ const locations = {
     "Wednesday, December 11": [
         { name: "West New York", coords: [-74.0143, 40.7870], color: "blue" }
     ],
+    
     "Thursday, December 12": [
         { name: "Rockefeller Center", coords: [-73.9787, 40.7587], color: "green" },
-        { name: "Central Park", coords: [-73.9683, 40.7851], color: "green" },
-        { name: "Chinatown", coords: [-73.9970, 40.7158], color: "green" },
-        { name: "Little Italy", coords: [-73.9973, 40.7191], color: "green" },
-        { name: "Brooklyn Bridge", coords: [-73.9969, 40.7061], color: "green" },
-        { name: "Marquis Theatre (Elf)", coords: [-73.9864, 40.7590], color: "green" },
         { name: "Rockefeller Christmas Tree", coords: [-73.9787, 40.7587], color: "green" },
-        { name: "Rockefeller Skating Rink", coords: [-73.9787, 40.7587], color: "green" }
+        { name: "Rockefeller Skating Rink", coords: [-73.9787, 40.7587], color: "green" },
+        { name: "Marquis Theatre (Elf)", coords: [-73.9864, 40.7590], color: "green" }
     ],
+    
     "Friday, December 13": [
-        { name: "9/11 Memorial & Museum", coords: [-74.0134, 40.7115], color: "red" },
-        { name: "New York Public Library", coords: [-73.9822, 40.7532], color: "red" },
-        { name: "Bryant Park", coords: [-73.9832, 40.7536], color: "red" },
-        { name: "Empire State Building", coords: [-73.9857, 40.7484], color: "red" },
         { name: "Times Square", coords: [-73.9855, 40.7580], color: "red" },
-        { name: "Grand Central Station", coords: [-73.9772, 40.7527], color: "red" }
+        { name: "Empire State Building", coords: [-73.9857, 40.7484], color: "red" },
+        { name: "Bryant Park", coords: [-73.9832, 40.7536], color: "red" },
+        { name: "New York Public Library", coords: [-73.9822, 40.7532], color: "red" }
     ],
+    
     "Saturday, December 14": [
-        { name: "Liberty Island", coords: [-74.0445, 40.6892], color: "purple" },
+        { name: "9/11 Memorial & Museum", coords: [-74.0134, 40.7115], color: "purple" },
+        { name: "Brooklyn Bridge", coords: [-73.9969, 40.7061], color: "purple" },
         { name: "Ellis Island", coords: [-74.0403, 40.6995], color: "purple" },
-        { name: "Chelsea Market", coords: [-74.0061, 40.7425], color: "purple" },
-        { name: "The High Line", coords: [-74.0049, 40.7479], color: "purple" },
-        { name: "Hudson Yards", coords: [-74.0017, 40.7546], color: "purple" },
-        { name: "The Edge Observatory", coords: [-74.0011, 40.7535], color: "purple" }
+        { name: "Liberty Island", coords: [-74.0445, 40.6892], color: "purple" }
     ],
+    
     "Sunday, December 15": [
         { name: "Saks Fifth Avenue", coords: [-73.9742, 40.7602], color: "orange" },
-        { name: "Aire Ancient Baths", coords: [-74.0022, 40.7174], color: "orange" },
-        { name: "Macy's Herald Square", coords: [-73.9890, 40.7508], color: "orange" }
+        { name: "Chelsea Market", coords: [-74.0061, 40.7425], color: "orange" },
+        { name: "The High Line", coords: [-74.0049, 40.7479], color: "orange" },
+        { name: "Hudson Yards", coords: [-74.0017, 40.7546], color: "orange" },
+        { name: "The Edge Observatory", coords: [-74.0011, 40.7535], color: "orange" }
+    ],
+
+    "Monday, December 16": [
+        { name: "LGA Airport", coords: [-73.8726, 40.7769], color: "yellow" }
     ]
 };
