1f94bd426b44798ee178fa0ad23bf5fe6de8bdc5
Anthony <anthony.notgrass@gmail.com>
2024-09-18 22:15:09 -0500

    Update markers.js

--- markers.js
+++ markers.js
@@ -7,25 +7,26 @@ const locations = {
         { name: "Rockefeller Center", coords: [-73.9787, 40.7587], color: "green" },
         { name: "Rockefeller Christmas Tree", coords: [-73.9787, 40.7587], color: "green" },
         { name: "Rockefeller Skating Rink", coords: [-73.9787, 40.7587], color: "green" },
-        { name: "Marquis Theatre (Elf)", coords: [-73.9864, 40.7590], color: "green" }
+        { name: "Central Park", coords: [-73.9683, 40.7851], color: "green" },
+        { name: "Saks Fifth Avenue", coords: [-73.9742, 40.7602], color: "green" }
     ],
-    
+
     "Friday, December 13": [
-        { name: "Times Square", coords: [-73.9855, 40.7580], color: "red" },
-        { name: "Empire State Building", coords: [-73.9857, 40.7484], color: "red" },
-        { name: "Bryant Park", coords: [-73.9832, 40.7536], color: "red" },
-        { name: "New York Public Library", coords: [-73.9822, 40.7532], color: "red" }
-    ],
-    
-    "Saturday, December 14": [
         { name: "9/11 Memorial & Museum", coords: [-74.0134, 40.7115], color: "purple" },
         { name: "Brooklyn Bridge", coords: [-73.9969, 40.7061], color: "purple" },
         { name: "Ellis Island", coords: [-74.0403, 40.6995], color: "purple" },
         { name: "Liberty Island", coords: [-74.0445, 40.6892], color: "purple" }
     ],
-    
+
+    "Saturday, December 14": [
+        { name: "Times Square", coords: [-73.9855, 40.7580], color: "red" },
+        { name: "Empire State Building", coords: [-73.9857, 40.7484], color: "red" },
+        { name: "Bryant Park", coords: [-73.9832, 40.7536], color: "red" },
+        { name: "New York Public Library", coords: [-73.9822, 40.7532], color: "red" },
+        { name: "Marquis Theatre (Elf)", coords: [-73.9864, 40.7590], color: "red" }
+    ],
+
     "Sunday, December 15": [
-        { name: "Saks Fifth Avenue", coords: [-73.9742, 40.7602], color: "orange" },
         { name: "Chelsea Market", coords: [-74.0061, 40.7425], color: "orange" },
         { name: "The High Line", coords: [-74.0049, 40.7479], color: "orange" },
         { name: "Hudson Yards", coords: [-74.0017, 40.7546], color: "orange" },
